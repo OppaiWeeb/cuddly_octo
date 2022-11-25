f1c24e20fa66fdcbb6f2511fee50c3357857256a
kawaegle <alecromski@gmail.com>
2022-11-25 19:07:18 +0100

    optimize for a GUI

--- src/window.c
+++ src/window.c
@@ -1,45 +1,28 @@
+#include <stdbool.h>
 #define RAYGUI_IMPLEMENTATION
 #include "../include/lib_led.h"
 #include "../sub/raygui/src/raygui.h"
 #include <raylib.h>
 
-window_t *init_window(void)
+static int update(window_t *window, bool *rect)
 {
-    struct window_s *window = malloc(sizeof(window_t));
-    if (!window)
-        return (NULL);
+    if (GuiButton((Rectangle){50, 180, window->width/5, window->height/4}, "Press me"))
+        *rect = !*rect;
 
-    window->width = 800;
-    window->height = 420;
-    window->window_name = strdup("Led generator");
-    InitWindow(window->width, window->height, window->window_name);
-    SetTargetFPS(60);
-    return (window);
-}
-
-static int print_text(bool *press, unsigned int time)
-{
-    if (*press && time >= 500)
-        *press = 0;
-    DrawText("JE SUIS UN TEXT", 100, 50, 25, GREEN);
     return (0);
 }
 
 int open_window(window_t *window)
 {
-    Vector2 mouse = GetMousePosition();
-    bool press = 0;
-    char button_pressed = 0;
-    unsigned int time = 0;
+    bool rectpress = false;
+    Rectangle rect = {10, 40, 50 ,50};
 
     while (!WindowShouldClose()) {
+        update(window, &rectpress);
         BeginDrawing();
-            if (GuiButton((Rectangle){ window->width - 170, window->height - 50, 150, 30 }, "press_me")) {
-                press = 1;
-                time = 0;
-            }
-            print_text(&press, time);
             ClearBackground(GRAY);
+        if (rectpress)
+            DrawRectangleRec(rect, RED);
         EndDrawing();
     }
     CloseWindow();
