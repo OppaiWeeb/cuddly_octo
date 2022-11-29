44ecddd4663552b326d33cf5a8484a3f926e4c06
kawaegle <alecromski@gmail.com>
2022-11-29 19:51:25 +0100

    work with windows scene for element of GUI update

--- src/window.c
+++ src/window.c
@@ -1,28 +1,67 @@
+#include <raylib.h>
 #include <stdbool.h>
+#include <stdio.h>
 #define RAYGUI_IMPLEMENTATION
 #include "../include/lib_led.h"
 #include "../include/raygui.h"
 
-static int update(window_t *window, bool *rect)
+static int update(window_t *window, generator_led_t *list)
 {
-    if (GuiButton((Rectangle){50, 180, window->width/5, window->height/4}, "Press me"))
-        *rect = !*rect;
-
+    switch (window->scene) {
+        case SELECT_FILE:
+            select_file_update(window, list);
+            break;
+        case WORK_ON_FILE:
+            work_on_file_update();
+            if (GuiButton((Rectangle){50, 180, window->width/5, window->height/4}, "Press me to save")) {
+                window->scene++;
+            }
+            break;
+        case END_AND_SAVE:
+            end_and_save_update();
+            if (GuiButton((Rectangle){50, 180, window->width/5, window->height/4}, "Press me to open file")) {
+                window->scene = SELECT_FILE;
+            }
+            break;
+    }
     return (0);
 }
 
+static int draw(window_t *window)
+{
+    BeginDrawing();
+    switch (window->scene) {
+        case SELECT_FILE:
+            select_file_window();
+            ClearBackground(RED);
+            break;
+        case WORK_ON_FILE:
+            work_on_file_window();
+            ClearBackground(GREEN);
+            break;
+        case END_AND_SAVE:
+            end_and_save_window();
+            ClearBackground(WHITE);
+            break;
+    }
+    EndDrawing();
+    return (0);
+}
+
+int work_on_file_update(void){return 0;}
+int end_and_save_update(void){return 0;}
+
+int select_file_window(void){return 0;}
+int work_on_file_window(void){return 0;}
+int end_and_save_window(void){return 0;}
+
 int open_window(window_t *window)
 {
-    bool rectpress = false;
-    Rectangle rect = {10, 40, 50 ,50};
+    generator_led_t *list = NULL;
 
     while (!WindowShouldClose()) {
-        update(window, &rectpress);
-        BeginDrawing();
-            ClearBackground(GRAY);
-        if (rectpress)
-            DrawRectangleRec(rect, RED);
-        EndDrawing();
+        update(window, list);
+        draw(window);
     }
     CloseWindow();
     free(window->window_name);
