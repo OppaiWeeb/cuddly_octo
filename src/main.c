2c40e95584539f0a43b9bf7ec2ab32684f6fc597
kawaegle <alecromski@gmail.com>
2022-11-29 19:50:52 +0100

    create file with GUI

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include "../include/lib_led.h"
-#include <stdlib.h>
 
 static window_t *init_window(void)
 {
@@ -10,44 +9,23 @@ static window_t *init_window(void)
     window->width = 800;
     window->height = 420;
     window->window_name = strdup("Cuddly Octo");
+    window->scene = SELECT_FILE;
     InitWindow(window->width, window->height, window->window_name);
     SetTargetFPS(60);
     return (window);
 }
 
-static generator_led_t *generate_led(char *const filename)
+int main(int argc, char *argv[])
 {
-    generator_led_t *led = malloc(sizeof(generator_led_t));
-    int ret = 0;
-
-    if (!led)
-        return (NULL);
-    ret = open_file(filename, led);
-    if (ret) {
-        free_led(led);
-        return (NULL);
-    }
-    return (led);
-}
-
-int main (int argc, char *argv[])
-{
-    generator_led_t *led = NULL;
     window_t *window = NULL;
 
-    if (argc > 2) {
-        printf("Usage %s <filename>\n", argv[0]);
+    if (argc > 1) {
+        printf("Usage %s\n", argv[0]);
         return (1);
     }
-    led = generate_led(argv[1]);
-    if (!led)
-        return (-1);
-    if (!led->fptr)
-        return (-2);
     window = init_window();
     if (!window)
-        free_led(led);
+        return (-1);
     open_window(window);
-    free_led(led);
     return (0);
 }
